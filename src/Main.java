7294e035d9d7175e1f7fa3d3ff890b29971c5bfc
jamester <jamester@students.cs.ubc.ca>
2020-04-03 20:33:32 -0700

    Mostly done, just tinker with sout lines

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Student jamie = Student.getStudent(1);
+        jamie.setName("Jamie Cohen");
+
+        Student pipe = Student.getStudent(2);
+        pipe.setName("Felipe Grosso");
+
+        Student rehan = Student.getStudent(3);
+        rehan.setName("Rehan Hafeez");
+        System.out.println("Student rehan's current name: " + rehan.getName());
+
+        Student imposterRehan = Student.getStudent(3);
+        imposterRehan.setName("Not Rehan :-0");
+        System.out.println("Student rehan's name: " + rehan.getName());
+        System.out.println("Student imposterRehan's name: " + imposterRehan.getName());
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,27 @@
+import java.util.HashMap;
+
 public class Student {
 
     private int id;
+    private String name;
+
+    private static HashMap<Integer, Student> studentMappings = new HashMap<>();
+
+    public static Student getStudent(int idNum) {
+        if (!studentMappings.containsKey(idNum)) studentMappings.put(idNum, new Student(idNum));
+        return studentMappings.get(idNum);
+    }
+
+    private Student(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
 
 }
